dd767855086907f0ecf945cd363d54ce9b76beed
Donchka <mhd993065259@gmail.com>
2025-08-11 22:34:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,6 @@ def add_item():
     return jsonify({"id": item.id, "name": item.name}), 201
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host="0.0.0.0", port=5000)
